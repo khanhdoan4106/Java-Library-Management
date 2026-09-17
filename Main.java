071a0c501659c2c1046862eaa3c44b565ca68086
Khanh Doan <dqkhanh412006@gmail.com>
2026-09-17 20:00:03 +0700

    Update comment for seedData method call

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        seedData(); // vài dữ liệu mẫu để test nhanh
+        seedData(); // vài dữ liệu mẫu để test nhanh, không bắt buộc
 
         boolean running = true;
         while (running) {
