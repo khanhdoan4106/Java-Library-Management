b71f25ed9bc98ef1439ac36095d33f62897f0c76
Khanh Doan <dqkhanh412006@gmail.com>
2026-09-17 19:59:02 +0700

    Update Ebook.java

--- Ebook.java
+++ Ebook.java
@@ -13,22 +13,12 @@ public class Ebook extends Book {
     }
 
     @Override
-    public boolean isAvailable() {
-        return true; // không giới hạn số bản, luôn có thể mượn
-    }
-
-    @Override
-    public void setAvailable(boolean available) {
-        // không làm gì cả -- ebook không có khái niệm "hết bản"
-    }
-
-    @Override
     public int getBorrowDurationInDays() {
         return 7; // ebook mượn ngắn hơn vì không giới hạn số bản
     }
 
     @Override
     public String getInfo() {
-        return super.getInfo() + String.format(" | Link: %s", downloadLink);
+        return super.getInfo() + String.format(" (Always available) | Link: %s", downloadLink);
     }
 }
